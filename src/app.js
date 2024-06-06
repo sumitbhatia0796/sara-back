9d58b66ccf8a545951dd2b42746e1b195ec8ba14
sumitbhatia0796 <sumitbhatia0796@gmail.com>
2024-06-06 21:54:55 +0530

    Core policy change

--- src/app.js
+++ src/app.js
@@ -25,16 +25,15 @@ const PORT = options.port;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
+app.use(cors({
+  origin: 'https://www.sarafurniture.info', // Replace with your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 if (process.env.NODE_ENV === 'development') {
   app.use(require('morgan')('dev'));
 }
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 app.use(helmet());
 
 // Serve static files
