912ceccfca657698cb83d9b8a08bf370331fd2b8
sumitbhatia0796 <sumitbhatia0796@gmail.com>
2024-06-06 22:43:42 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -34,12 +34,7 @@ app.use(cors(corsOptions));
 if (process.env.NODE_ENV === 'development') {
   app.use(require('morgan')('dev'));
 }
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 app.use(helmet());
 
 // Serve static files
